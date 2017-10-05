581cb935e9a0524d4f1833769b01acae6f4048fb
Say Sotheara <saysotheara@Says-MacBook-Pro.local>
2017-10-05 10:21:02 +0700

    initial upload

---
+++ api/index.php
@@ -0,0 +1,121 @@
+<?php
+
+require 'Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+function getConnection() {
+    try {
+        $db_username = "usr_kamcourse";
+        $db_password = "pwd_kamcourse";
+        $conn = new PDO('mysql:host=localhost;dbname=nkk-db-kamcourse;charset=utf8', $db_username, $db_password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+    return null;
+}
+
+function responseJSON($sql_query) {
+    try {
+        $dbCon = getConnection();
+        $stmt  = $dbCon->query($sql_query);
+        $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $dbCon = null;
+        echo json_encode($results, JSON_NUMERIC_CHECK);
+    }
+    catch(PDOException $e) {
+        echo '{"error": {"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function responseJSON_ID($sql_query, $id) {
+    try {
+        $dbCon = getConnection();
+        $stmt = $dbCon->prepare($sql_query);  
+        $stmt->bindParam("id", $id);
+        $stmt->execute();
+        $results = $stmt->fetchObject();  
+        $dbCon = null;
+        echo json_encode($results, JSON_NUMERIC_CHECK);
+    }
+    catch(PDOException $e) {
+        echo '{"error": {"text":'. $e->getMessage() .'}}';
+    }
+}
+
+$app->get('/course', function() {
+    $sql_query = "SELECT * FROM kc_tbl_course";
+    responseJSON( $sql_query );
+});
+
+$app->get('/course/:id', function($id) {
+    $sql_query = "SELECT * FROM kc_tbl_course WHERE course_id = :id";
+    responseJSON_ID( $sql_query, $id);
+});
+
+$app->post('/course', function() {
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+    try {
+        $sql_query = "INSERT INTO kc_tbl_course (course_name, course_summary, course_category, course_outline, course_duration, course_fee, course_media, course_video, course_create_date, course_update_date, course_other_info) VALUES (:name, :summary, :category, :outline, :duration, :fee, :photo_url, :video_url, :create_date, :update_date, :other_info)";
+        $dbCon = getConnection();
+        $stmt = $dbCon->prepare($sql_query);
+        $stmt->bindParam("id", $request->id);
+        $stmt->bindParam("name", $request->name);
+        $stmt->bindParam("summary", $request->summary);
+        $stmt->bindParam("category", $request->category);
+        $stmt->bindParam("outline", $request->outline);
+        $stmt->bindParam("duration", $request->duration);
+        $stmt->bindParam("fee", $request->fee);
+        $stmt->bindParam("photo_url", $request->photo_url);
+        $stmt->bindParam("video_url", $request->video_url);
+        $stmt->bindParam("create_date", NOW());
+        $stmt->bindParam("update_date", NOW());
+        $stmt->bindParam("other_info", $request->other_info);
+        $stmt->execute();
+        $dbCon = null;
+    }
+    catch(PDOException $e) {
+        echo '{"error": {"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$app->put('/course/:id', function($id) {
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+    try {
+        $sql_query = "UPDATE kc_tbl_course SET course_name = :name, course_summary = :summary, course_category = :category, course_outline = :outline, course_duration = :duration, course_fee = :fee, course_media = :photo_url, course_update_date = :update_date, course_other_info = :other_info WHERE course_id = :id";
+        $dbCon = getConnection();
+        $stmt = $dbCon->prepare($sql_query);
+        $stmt->bindParam("id", $request->id);
+        $stmt->bindParam("name", $request->name);
+        $stmt->bindParam("summary", $request->summary);
+        $stmt->bindParam("category", $request->category);
+        $stmt->bindParam("outline", $request->outline);
+        $stmt->bindParam("duration", $request->duration);
+        $stmt->bindParam("fee", $request->fee);
+        $stmt->bindParam("photo_url", $request->photo_url);
+        $stmt->bindParam("video_url", $request->video_url);
+        $stmt->bindParam("update_date", NOW());
+        $stmt->bindParam("other_info", $request->other_info);
+        $stmt->execute();
+        $dbCon = null;
+    }
+    catch(PDOException $e) {
+        echo '{"error": {"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$app->delete('/course/:id', function($id) {
+    $sql_query = "DELETE * FROM kc_tbl_course WHERE course_id = :id";
+    responseJSON_ID( $sql_query, $id);
+});
+
+
+$app->run();
+
+?>
