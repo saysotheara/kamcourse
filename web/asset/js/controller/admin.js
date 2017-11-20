cc513338395608afa65f918be4b11635fd689457
=hai-Ratana <=>
2017-11-20 16:43:15 +0700

    update cookies login Info

--- web/asset/js/controller/admin.js
+++ web/asset/js/controller/admin.js
@@ -1,11 +1,20 @@
-app.controller("loginCtrl",function($scope,$routeParams,$http,$location,$rootScope,$cookieStore){
-
+app.controller("loginCtrl",function($scope,$routeParams,$http,$location,$rootScope,$cookieStore,$route){
+  $scope.hasLogIn = $cookieStore.get('loggedIn');
   $scope.loginAdmin = function(){
     if($scope.username == 'admin' && $scope.password == '1234'){
+
       $cookieStore.put('loggedIn',true);
       $location.path('/admin/course');
 
     }
   };
+  $scope.logout = function(){
+    
+    $cookieStore.remove('loggedIn');
+    $location.path('/admin');
+
+  };
+
+
 
 });
--- web/asset/js/service/route-config.js
+++ web/asset/js/service/route-config.js
@@ -39,36 +39,99 @@ app.config(function($routeProvider,$locationProvider){
         controller: "CourseController"
     // Class Management
     }).when('/admin/class',{
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
         templateUrl: "template/class/class-list.html",
         controller: "ClassController",
         activetab: 'class'
 
     }).when('/admin/class/add',{
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
         templateUrl: "template/class/class-add.html",
         controller: "ClassController"
     }).when('/admin/class/update',{
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
         templateUrl: "template/class/class-update.html",
         controller: "ClassController"
 
         // Student
     }).when('/admin/createstudent',{
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
     templateUrl: "template/student/create_student.html",
     controller: "StudentController"
      }).when('/admin/student',{
+       resolve:{
+         "check": function($location,$cookieStore){
+           if(!$cookieStore.get('loggedIn')){
+             $location.path('/admin');
+           }
+         }
+       },
     templateUrl: "template/student/student.html",
     controller: "StudentController",
     activetab: 'student'
      }).when('/admin/studentup/:id',{
+       resolve:{
+         "check": function($location,$cookieStore){
+           if(!$cookieStore.get('loggedIn')){
+             $location.path('/admin');
+           }
+         }
+       },
     templateUrl: "template/student/update_student.html",
     controller: "StudentController"
       // facilitator
    }).when('/admin/createfacilitator',{
+     resolve:{
+       "check": function($location,$cookieStore){
+         if(!$cookieStore.get('loggedIn')){
+           $location.path('/admin');
+         }
+       }
+     },
     templateUrl: "template/facilitator/create_facilitator.html",
     controller: "FacilitatorController"
      }).when('/admin/facilitator',{
+       resolve:{
+         "check": function($location,$cookieStore){
+           if(!$cookieStore.get('loggedIn')){
+             $location.path('/admin');
+           }
+         }
+       },
     templateUrl: "template/facilitator/facilitator.html",
     controller: "FacilitatorController"
      }).when('/admin/updatefacilitator/:id',{
+       resolve:{
+         "check": function($location,$cookieStore){
+           if(!$cookieStore.get('loggedIn')){
+             $location.path('/admin');
+           }
+         }
+       },
     templateUrl: "template/facilitator/update_facilitator.html",
     controller: "FacilitatorController"
 
@@ -78,32 +141,67 @@ app.config(function($routeProvider,$locationProvider){
   });
     //category
     $routeProvider.when('/admin/category/create',{
-      // resolve:{
-      //   "check": function($location,$rootScope){
-      //     if(!$rootScope.loggedIn){
-      //       $location.path('/admin');
-      //     }
-      //   }
-      // },
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
       templateUrl: "template/category/create.html",
       controller: "categoryCtl"
     }).when('/admin/category',{
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
       templateUrl: "template/category/category.html",
       controller: "categoryCtl",
       activetab: 'category'
     }).when('/admin/category/update/:id',{
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
       templateUrl: 'template/category/update.html',
       controller: 'categoryCtl'
     });
     //schedule
     $routeProvider.when('/admin/schedule',{
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
       templateUrl:'template/schedule/schedule.html',
       controller: 'categoryCtl',
       activetab: 'schedule'
     }).when('/admin/schedule/update/:id',{
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
       templateUrl: 'template/schedule/update.html',
       controller: 'categoryCtl'
     }).when('/admin/schedule/create',{
+      resolve:{
+        "check": function($location,$cookieStore){
+          if(!$cookieStore.get('loggedIn')){
+            $location.path('/admin');
+          }
+        }
+      },
       templateUrl: 'template/schedule/create.html',
       controller: 'categoryCtl'
     });
